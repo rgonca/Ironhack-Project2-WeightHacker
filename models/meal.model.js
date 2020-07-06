3603a4301c2c4b8b4ada9b0fdc497fe571eee9be
Míriam Sánchez Alcón <msanchezalcon.msa@gmail.com>
2020-07-06 12:37:23 +0200

    deleted extra models

--- models/meal.model.js
+++ models/meal.model.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
-const ingredientSchema = new Schema({
+const mealSchema = new Schema({
     name: String,
     kcal: Number
 }, {
     timestamps: true
 })
 
-const Ingredient = mongoose.model("Ingredient", ingredientSchema)
+const Meal = mongoose.model("Meal", mealSchema)
 
-module.exports = Ingredient
+module.exports = Meal
--- models/user.model.js
+++ models/user.model.js
@@ -11,3 +11,48 @@ const userSchema = new Schema({
 const User = mongoose.model("User", userSchema)
 
 module.exports = User
+
+
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+
+// const profileSchema = new Schema({
+//     name: String,
+//     picture: { type: Schema.Types.ObjectId, ref: 'Picture' },
+//     sex: {
+//         type: String,
+//         enum: ['male', 'female']
+//     },
+//     idealweight: Number
+// })
+
+// const Profile = mongoose.model("Profile", profileSchema)
+
+// module.exports = Profile
+
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+
+// const pictureSchema = new Schema({
+//     name: String,
+//     path: String,
+//     originalName: String
+// }, {
+//     timestamps: true
+// })
+
+// const Picture = mongoose.model("Picture", pictureSchema)
+// module.exports = Picture
+
+// const mongoose = require("mongoose")
+// const Schema = mongoose.Schema
+
+// const weightSchema = new Schema({
+//     weight: Number
+// }, {
+//     timestamps: true
+// })
+
+// const Weight = mongoose.model("Weight", weightSchema)
+
+// module.exports = Weight
