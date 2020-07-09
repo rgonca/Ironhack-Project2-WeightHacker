a48d4651d2f841cd68f2bb2339d362a3a4e57ddd
Míriam Sánchez Alcón <msanchezalcon.msa@gmail.com>
2020-07-09 14:38:12 +0200

    working on showing meal table

--- public/javascripts/calculator.js
+++ public/javascripts/calculator.js
@@ -1,17 +1,17 @@
 
 function onMealWeightChange() {
     console.log('traza', 'yo calculo las calorias')
-    
+
     const weight = document.querySelector('#mealWeight input').value
     console.log('traza', weight)
     const calories = document.querySelector('.calories span').innerHTML
     const serving = document.querySelector('.serving span').innerHTML
-    
-    
-    
+
+
+
     let totalCalories = (weight / serving) * calories
-    
-    document.querySelector('.totalCalories span').innerHTML = totalCalories
+
+    document.querySelector('.totalCalories span').innerHTML = totalCalories.toFixed(2)
     document.querySelector('#kcal').value = totalCalories
     document.querySelector('#amount_gr').value = weight
     return totalCalories
