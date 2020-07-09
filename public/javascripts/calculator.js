ffa7287e9d60f77fe4d769a4d89b26c911eb2bbb
Míriam Sánchez Alcón <msanchezalcon.msa@gmail.com>
2020-07-10 00:06:31 +0200

    function for sum of total kcal

--- public/javascripts/calculator.js
+++ public/javascripts/calculator.js
@@ -1,10 +1,9 @@
 
+//Calculator funcion
 function onMealWeightChange() {
     const weight = document.querySelector('.mealWeight input').value
     const calories = document.querySelector('.calories span').innerHTML
     const serving = document.querySelector('.serving span').innerHTML
-
-
 
     let totalCalories = (weight / serving) * calories
 
@@ -12,14 +11,16 @@ function onMealWeightChange() {
     document.querySelector('#kcal').value = totalCalories
     document.querySelector('#amount_gr').value = weight
     return totalCalories
-};
-
-
-function calorieCount() {
-    const calorieCounter = document.querySelectorAll(".allKcal").value
-    let totalKcal = calorieCounter.forEach(element => {((a, b) => a + b, 0)})
-    return totalKcal
 }
+
+// Sum all total kcal
+const calorieCount = () => {
+    let total = 0
+    const calorieCounter = document.querySelectorAll(".allKcal").value
+    calorieCounter.forEach(elm => total += elm)
+    return total
+}
+
 
 // const calculateCaloriesBtn = document.getElementById('calculate')
 // calculateCaloriesBtn.addEventListener('click', onCalculateButtonClick)
